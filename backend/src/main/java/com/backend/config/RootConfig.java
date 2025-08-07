e99a12da218fc8b7cc42f0bdac3f641893dcc51c
KessokuMAS <cccc5606@gmail.com>
2025-08-07 15:52:10 +0900

    Delete backend directory

--- backend/src/main/java/com/backend/config/RootConfig.java
+++
@@ -1,6 +0,0 @@
-package com.backend.config;
-
-
-public class RootConfig {
-    
-}
--- backend/src/main/java/com/backend/domain/member/MemberRole.java
+++
@@ -1,7 +0,0 @@
-package com.backend.domain.member;
-
-public enum MemberRole {
-    USER,           // 일반 사용자
-    BUSINESS_OWNER, // 소상공인
-    ADMIN           // 관리자
-}
